ca4d248e7830ff7d593824ef3f2616e594575d42
lharrak-botaina <lharrak.botaina@gmail.com>
2023-02-21 16:59:30 +0100

    exmple:recyclerView

---
+++ recyclerView/app/src/main/java/com/example/list/adapter/ItemAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.list.adapter
+
+import com.example.list.model.Affirmation
+
+class ItemAdapter(private val dataset:List<Affirmation>) {
+}
---
+++ recyclerView/app/src/main/java/com/example/list/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.example.list.model
+
+data class Affirmation (val stringResourceId:Int){
+}
